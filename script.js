d617b12ed212bb5cccfeae9050ce38e03e97e156
Grayson Keaton <gkeaton23@gmail.com>
2024-08-30 01:39:09 +0300

    done task 3

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 window.addEventListener("load", function() {
-    console.log("Window loaded");
-
-    // let listedPlanets;
-    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    // let listedPlanetsResponse;
-    // listedPlanetsResponse.then(function (result) {
-    //     listedPlanets = result;
-    //     console.log(listedPlanets);
-    // }).then(function () {
-    //     console.log(listedPlanets);
-    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    // })
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        // console.log(listedPlanets);
+    }).then(function () {
+        // console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let planet = pickPlanet(listedPlanets);
+        console.log(planet);
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
+    })
 
     function formSubmit(event) {
         event.preventDefault();
